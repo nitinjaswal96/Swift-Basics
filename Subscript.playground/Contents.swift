f4e12dc6bdd5f1a624ae4f52c7a19657afcca23d
Nitin Jaswal <nitinjaswal96@gmail.com>
2021-08-14 20:14:39 -0400

    commit

---
+++ Subscript.playground/Contents.swift
@@ -0,0 +1,22 @@
+import UIKit
+
+// Subscript is a shortcut for accessing the member elements of a collection, list, or sequence.
+
+struct Classroom {
+    var students = [["Sam", "John", "Bella", "Steve"], ["Sabrina", "Dave", "Charlie"]]
+    
+    subscript(row: Int, col: Int) -> String {
+        get {
+            return students[row][col]
+        }
+        set {
+            students[row][col] = newValue
+        }
+    }
+}
+
+var clas = Classroom()
+clas[1, 0]
+clas[0, 0]
+clas[0, 0] = "Mahesh"
+clas[0, 0]
